feaa515415f44f3fce0d50de334a3c14bc6c53d4
kryanbz <kryan@ub.edu.bz>
2022-04-25 10:33:07 -0600

    Update firebaseInit.js

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -2,13 +2,7 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCVgqh-H-fALWhTfHwc6M3N6h9zN8Bzdmg",
-    authDomain: "bzeducators.firebaseapp.com",
-    projectId: "bzeducators",
-    storageBucket: "bzeducators.appspot.com",
-    messagingSenderId: "624621682942",
-    appId: "1:624621682942:web:c28b88c0d91b7d149ca5af",
-    measurementId: "G-S8JSSGP3RJ"
+
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
